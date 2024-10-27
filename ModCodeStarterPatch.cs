3f1344a5603fbca3c4ef826993f137ed450238c7
Norman Rasmussen <norman@rasmussen.co.za>
2024-10-26 19:35:11 -0700

    Try and avoid a rare crash
    There is a rare NullReferenceException happening in StartMod that is
    hard to reproduce, if it is because of mod being null this fix should
    avoid it causing a crash.

--- ModCodeStarterPatch.cs
+++ ModCodeStarterPatch.cs
@@ -10,13 +10,13 @@ namespace Mods.SteamUpdateButtons {
   [HarmonyPatch("StartMod")]
   static class ModCodeStarterPatch {
     static void Prefix(Mod mod, out DateTime __state) {
-      Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + mod.DisplayName + ": Starting from " + (mod?.ModDirectory.Path ?? "an unknown location"));
+      Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + (mod?.DisplayName ?? "Unknown mod") + ": Starting from " + (mod?.ModDirectory.Path ?? "an unknown location"));
       __state = DateTime.Now;
     }
 
     static void Postfix(Mod mod, DateTime __state) {
       var duration = DateTime.Now - __state;
-      Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + mod.DisplayName + ": Started in " + duration);
+      Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + (mod?.DisplayName ?? "Unknown mod") + ": Started in " + duration);
     }
   }
 }
