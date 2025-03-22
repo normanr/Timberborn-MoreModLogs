b6893c143ab5fe5b47e2b2a37974997f4b6855f7
Norman Rasmussen <norman@rasmussen.co.za>
2025-03-21 17:52:29 -0700

    Log additional context if blueprint deserialization fails

---
+++ SpecServicePatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using HarmonyLib;
+using System.Linq;
+
+namespace Mods.MoreModLogs {
+
+  [HarmonyPatch]
+  static class SpecServicePatch {
+
+    public static MethodBase TargetMethod() {
+      return AccessTools.TypeByName("Timberborn.BlueprintSystem.SpecService").Method("DeserializeBlueprint");
+    }
+
+    static void Finalizer(IGrouping<string, TextAsset> jsons, Exception __exception) {
+      if (__exception == null) return;
+      Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + "Failed to deserialize blueprint for " + jsons.Key);
+      foreach (var item in jsons) {
+        Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + item.name + ":\n" + item.text);
+      }
+    }
+  }
+}
