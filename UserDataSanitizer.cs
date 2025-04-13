423a50edb988cd6bb0fe010a96d5a1fbca2c3e94
Norman Rasmussen <norman@rasmussen.co.za>
2025-04-13 14:36:31 -0700

    Remove user specific data from paths

---
+++ UserDataSanitizer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mods.MoreModLogs {
+  public static class UserDataSanitizer {
+
+    public static string Sanitize(string path) {
+      var myDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+      if (path.StartsWith(myDocuments, StringComparison.InvariantCulture)) {
+        path = "..." + path.Substring(myDocuments.Length);
+      }
+      return path;
+    }
+
+  }
+}
