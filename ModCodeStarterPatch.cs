c2b31b10787c9263b2750484344e13eb0fd7e4ab
Norman Rasmussen <norman@rasmussen.co.za>
2025-05-11 18:53:55 -0700

    Add log entry with Minimum Game Versions for all mods on startup

--- ModCodeStarterPatch.cs
+++ ModCodeStarterPatch.cs
@@ -3,13 +3,29 @@ using UnityEngine;
 using HarmonyLib;
 using Timberborn.Modding;
 using Timberborn.ModManagerScene;
+using System.Linq;
 
 namespace Mods.MoreModLogs {
 
   [HarmonyPatch(typeof(ModCodeStarter))]
   [HarmonyPatch("StartMod")]
   static class ModCodeStarterPatch {
-    static void Prefix(Mod mod, out DateTime __state) {
+
+    static bool initalized;
+
+    private static void FirstRun(ModRepository modRepository) {
+      Debug.Log("Minimum Game Versions:");
+      foreach (var mod in modRepository.EnabledMods
+                          .OrderBy((mod) => mod.Manifest.MinimumGameVersion.Numeric)) {
+        Debug.Log("- " + mod.Manifest.MinimumGameVersion.Numeric + " - " + mod.Manifest.Name);
+      }
+    }
+
+    static void Prefix(Mod mod, ModRepository ____modRepository, out DateTime __state) {
+      if (!initalized) {
+        FirstRun(____modRepository);
+        initalized = true;
+      }
       __state = DateTime.Now;
     }
 
@@ -20,7 +36,7 @@ namespace Mods.MoreModLogs {
       if (__exception == null) {
         Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + displayName + ": Started from " + modPath + " in " + duration);
       } else {
-        Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + displayName + ": Failed to start from " + modPath + " after " + duration);
+        Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + displayName + ": Failed to start from " + modPath + " after " + duration);
       }
     }
   }
