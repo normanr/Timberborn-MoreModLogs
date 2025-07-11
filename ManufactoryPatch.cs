80f24fbdaca3b92716151425e42a8199f1dc0864
Norman Rasmussen <norman@rasmussen.co.za>
2025-07-11 16:47:26 -0700

    Add logging for missing RecipeSpec BackwardCompatibleIds

---
+++ ManufactoryPatch.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using HarmonyLib;
+using Timberborn.Workshops;
+
+namespace Mods.MoreModLogs {
+
+  [HarmonyPatch(typeof(Manufactory))]
+  [HarmonyPatch(nameof(Manufactory.Load))]
+  static class ManufactoryPatch {
+
+    static void Finalizer(Exception __exception, Manufactory __instance) {
+      if (__exception == null) return;
+      Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + "Manufactory.Load failed with an exception");
+      foreach (var recipe in __instance.ProductionRecipes) {
+        if (recipe.BackwardCompatibleIds.IsDefault) {
+          Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + "  " + recipe.Id + " is missing BackwardCompatibleIds");
+        }
+      }
+    }
+
+  }
+}
