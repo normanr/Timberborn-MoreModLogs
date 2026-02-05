6d695d4ee7a828d7ea6f9a55b8df6452f55d410e
Norman Rasmussen <norman@rasmussen.co.za>
2026-02-04 20:54:03 -0800

    Add additional context to failures in PlantingToolButtonFactory.GetPlanterBuildingName

---
+++ PlantingToolPatch.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using HarmonyLib;
+using Timberborn.Planting;
+using Timberborn.PlantingUI;
+using Timberborn.TemplateSystem;
+
+namespace Mods.MoreModLogs {
+
+  [HarmonyPatch(typeof(PlantingToolButtonFactory))]
+  [HarmonyPatch("GetPlanterBuildingName")]
+  static class PlantingToolPatch {
+
+    static void Finalizer(Exception __exception, PlantableSpec plantableSpec) {
+      if (__exception == null) return;
+      var templateSpec = plantableSpec.GetSpec<TemplateSpec>();
+      Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + $"PlantingToolButtonFactory.GetPlanterBuildingName({templateSpec.TemplateName} looking for {plantableSpec.ResourceGroup}) failed with an exception");
+    }
+
+  }
+}
