95e9e71c7e954bfd94cbb8bb7d673657e00eb1e8
Norman Rasmussen <norman@rasmussen.co.za>
2026-02-13 15:04:43 -0800

    Warn if blueprint specs are not an object

--- BlueprintDeserializerPatch.cs
+++ BlueprintDeserializerPatch.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using HarmonyLib;
 using Timberborn.BlueprintSystem;
-using System.Collections.Generic;
-using System.Linq;
+using Timberborn.SerializationSystem;
 
 namespace Mods.MoreModLogs {
 
   [HarmonyPatch(typeof(BlueprintDeserializer))]
-  [HarmonyPatch(nameof(BlueprintDeserializer.DeserializeUnsafe))]
   static class BlueprintDeserializerPatch {
 
     static List<string> _warnings;
 
-    static void Prefix() {
+    [HarmonyPatch(nameof(BlueprintDeserializer.DeserializeUnsafe))]
+    [HarmonyPrefix()]
+    static void DeserializeUnsafePrefix() {
       _warnings = [];
     }
 
@@ -21,18 +23,33 @@ namespace Mods.MoreModLogs {
       _warnings?.Add(warning);
     }
 
-    static void Finalizer(BlueprintFileBundle blueprintFileBundle, Exception __exception) {
+    [HarmonyPatch(nameof(BlueprintDeserializer.DeserializeUnsafe))]
+    [HarmonyFinalizer()]
+    static void DeserializeUnsafeFinalizer(BlueprintFileBundle blueprintFileBundle, Exception __exception) {
       if (__exception == null && _warnings.Count == 0) return;
       var bundleName = blueprintFileBundle.Path + " (" + string.Join(", ", blueprintFileBundle.Sources) + ")";
       if (__exception != null) {
         Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"Failed to deserialize blueprint for {bundleName}");
-      } else if (_warnings.Count > 0) {
+      }
+      if (_warnings.Count > 0) {
         Debug.LogWarning(DateTime.Now.ToString("HH:mm:ss ") + $"Blueprint deserializer for {bundleName} generated warnings:");
       }
       foreach (var item in _warnings.GroupBy(k => k)) {
         Debug.LogWarning(DateTime.Now.ToString("HH:mm:ss ") + $"- {item.Count()} {item.Key}");
       }
       _warnings = null;
+    }
+
+    [HarmonyPatch("DeserializeSpecs")]
+    [HarmonyPrefix()]
+    static void DeserializeSpecsPrefix(SerializedObject serializedObject) {
+      foreach (string item in serializedObject.Properties())
+      {
+        var obj = serializedObject.GetSerialized(item);
+        if (obj is not SerializedObject) {
+          AddWarning($"*** {item} has the wrong type: {obj.GetType()} != SerializedObject");
+        }
+      }
     }
   }
 }
