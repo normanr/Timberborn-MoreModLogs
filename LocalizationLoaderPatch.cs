94657dd975083d37197c4fab637ccba829b7961e
Norman Rasmussen <norman@rasmussen.co.za>
2026-02-13 15:07:05 -0800

    Add a warning if a localization value contains a strikethrough tag

---
+++ LocalizationLoaderPatch.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Mods.MoreModLogs {
+
+  [HarmonyPatch]
+  static class LocalizationLoaderPatch {
+
+    public static MethodBase TargetMethod() {
+      return AccessTools.TypeByName("Timberborn.Localization.LocalizationLoader").Method("GetLocalization");
+    }
+
+    static void Postfix(Dictionary<string, string> __result) {
+      foreach (var (key, value) in __result) {
+        if (value.Contains("<s>", StringComparison.InvariantCultureIgnoreCase))
+        Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + $"*** Localization {key} contains \"<s>\": {value}");
+      }
+    }
+  }
+}
