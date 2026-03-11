f502403d4996c4cbeb0ae6b4aba3facf7db79021
Norman Rasmussen <norman@rasmussen.co.za>
2026-03-10 20:54:57 -0700

    Add mod sources to errors logged when blueprints fail to read or are duplicated

--- Scripts/TemplateNameMapperPatch.cs
+++ Scripts/TemplateNameMapperPatch.cs
@@ -11,9 +11,29 @@ namespace Mods.MoreModLogs {
     public static void Finalizer(TemplateNameMapper __instance, string templateName, TemplateSpec templateSpec, Exception __exception) {
       if (__exception == null) return;
       if (__instance.TryGetTemplate(templateName, out var templateSpec2)) {
-        Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + $"TemplateNameMapper.TryAddTemplate({templateName}) failed with duplicate templates from:");
-        Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + $"- {Singletons.BlueprintSourceService?.Get(templateSpec2.Blueprint)?.Path}");
-        Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + $"- {Singletons.BlueprintSourceService?.Get(templateSpec.Blueprint)?.Path}");
+        Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"TemplateNameMapper.TryAddTemplate({templateName}) failed with duplicate templates from:");
+        try {
+          var bp2 = Singletons.BlueprintSourceService?.Get(templateSpec2.Blueprint);
+          if (bp2 != null) {
+            Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- {bp2?.Path} ({string.Join(", ", bp2?.Sources ?? new())})");
+          } else {
+            Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- mystery location");
+          }
+        }
+        catch {
+          Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- unknown location");
+        }
+        try {
+          var bp = Singletons.BlueprintSourceService?.Get(templateSpec.Blueprint);
+          if (bp != null) {
+            Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- {bp?.Path} ({string.Join(", ", bp?.Sources ?? new())})");
+          } else {
+            Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- mystery location");
+          }
+        }
+        catch {
+          Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- unknown location");
+        }
       }
     }
 
