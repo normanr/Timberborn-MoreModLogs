a545a8763faa5f15f3efd0edfd08046cb46d6fbd
Norman Rasmussen <norman@rasmussen.co.za>
2026-03-11 18:49:12 -0700

    Add exception context for BlockObjectToolButtonFactory.CreateTool

---
+++ Scripts/BlockObjectToolButtonFactoryPatch.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Reflection;
+using UnityEngine;
+using HarmonyLib;
+using Timberborn.BlockObjectToolsUI;
+using Timberborn.BlockSystem;
+using Timberborn.TemplateSystem;
+
+namespace Mods.MoreModLogs {
+
+  [HarmonyPatch]
+  static class BlockObjectToolButtonFactoryPatch {
+
+    public static MethodBase TargetMethod() {
+      return typeof(BlockObjectToolButtonFactory).Method("CreateTool");
+    }
+
+    static void Finalizer(PlaceableBlockObjectSpec template, Exception __exception) {
+      if (__exception == null) return;
+      try {
+        if (template == null) {
+          Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + "Failed to create BlockObjectTool for null PlaceableBlockObjectSpec");
+          return;
+        }
+        var bundle = Singletons.BlueprintSourceService.Get(template.Blueprint);
+        Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"Failed to create BlockObjectTool for {template?.GetSpec<TemplateSpec>()?.TemplateName} in {bundle.Path} ({string.Join(", ", bundle.Sources)})");
+      }
+      catch {
+      }
+      Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"Failed to create BlockObjectTool for {template?.GetSpec<TemplateSpec>()?.TemplateName} in unknown location");
+    }
+
+  }
+}
