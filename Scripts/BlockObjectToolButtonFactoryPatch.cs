395bb87763e679dccf60a2813807199347c53b2d
Norman Rasmussen <norman@rasmussen.co.za>
2026-03-15 23:07:35 -0700

    Fix extra log message in context for BlockObjectToolButtonFactory.CreateTool

--- Scripts/BlockObjectToolButtonFactoryPatch.cs
+++ Scripts/BlockObjectToolButtonFactoryPatch.cs
@@ -13,17 +13,17 @@ static class BlockObjectToolButtonFactoryPatch {
 
   static void Finalizer(PlaceableBlockObjectSpec template, Exception __exception) {
     if (__exception == null) return;
+    if (template == null) {
+      Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + "Failed to create BlockObjectTool for null PlaceableBlockObjectSpec");
+      return;
+    }
     try {
-      if (template == null) {
-        Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + "Failed to create BlockObjectTool for null PlaceableBlockObjectSpec");
-        return;
-      }
       var bundle = Singletons.BlueprintSourceService.Get(template.Blueprint);
       Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"Failed to create BlockObjectTool for {template?.GetSpec<TemplateSpec>()?.TemplateName} in {bundle.Path} ({string.Join(", ", bundle.Sources)})");
     }
     catch {
+      Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"Failed to create BlockObjectTool for {template?.GetSpec<TemplateSpec>()?.TemplateName} in unknown location");
     }
-    Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"Failed to create BlockObjectTool for {template?.GetSpec<TemplateSpec>()?.TemplateName} in unknown location");
   }
 
 }
