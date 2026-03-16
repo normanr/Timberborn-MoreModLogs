f09d9472066e0b59a62258b69bc2fc724cb7d2a5
Norman Rasmussen <norman@rasmussen.co.za>
2026-03-15 23:09:22 -0700

    Add exception context for AssetRef.Value

---
+++ Scripts/AssetRefPatch.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using HarmonyLib;
+using Timberborn.BlueprintSystem;
+
+namespace Mods.MoreModLogs;
+
+[HarmonyPatch(typeof(AssetRef<UnityEngine.Object>))]
+public static class AssetRefPatch {
+
+  [HarmonyPatch(MethodType.Constructor)]
+  [HarmonyPatch([typeof(string), typeof(Lazy<UnityEngine.Object>)])]
+  public static void Postfix(string path, ref Lazy<UnityEngine.Object> ____lazyAsset) {
+    var lazyAsset = ____lazyAsset;
+    ____lazyAsset = new(() => {
+      var startTime = DateTime.Now;
+      try {
+        return lazyAsset.Value;
+      }
+      catch (Exception) {
+        var duration = DateTime.Now - startTime;
+        Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + "AssetRef(" + path + ").Value failed after " + duration);
+        throw;
+      }
+    });
+  }
+}
