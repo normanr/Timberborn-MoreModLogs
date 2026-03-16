dbc1b77b2f525332d114f6c770ed1e4189fcde64
Norman Rasmussen <norman@rasmussen.co.za>
2026-03-15 23:56:59 -0700

    Add template collections to errors logged when blueprints fail to read or are duplicated

--- Scripts/TemplateNameMapperPatch.cs
+++ Scripts/TemplateNameMapperPatch.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using HarmonyLib;
+using Timberborn.TemplateCollectionSystem;
 using Timberborn.TemplateSystem;
 
 namespace Mods.MoreModLogs;
@@ -13,10 +14,19 @@ static class TemplateNameMapperPatch {
     if (__exception == null) return;
     if (__instance.TryGetTemplate(templateName, out var templateSpec2)) {
       Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"TemplateNameMapper.TryAddTemplate({templateName}) failed with duplicate templates from:");
+      var specService = __instance._templateService._templateCollectionService._specService;
       try {
         var bp2 = Singletons.BlueprintSourceService?.Get(templateSpec2.Blueprint);
         if (bp2 != null) {
           Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- {bp2?.Path} ({string.Join(", ", bp2?.Sources ?? new())})");
+          foreach (var spec in specService.GetSpecs<TemplateCollectionSpec>()) {
+            foreach (var blueprint in spec.Blueprints) {
+              if (blueprint.Path.Equals(bp2?.Path, StringComparison.InvariantCultureIgnoreCase)) {
+                var bp3 = Singletons.BlueprintSourceService?.Get(spec.Blueprint);
+                Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + $"  - {bp3?.Path} ({string.Join(", ", bp3?.Sources ?? new())})");
+              }
+            }
+          }
         } else {
           Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- mystery location");
         }
@@ -28,6 +38,14 @@ static class TemplateNameMapperPatch {
         var bp = Singletons.BlueprintSourceService?.Get(templateSpec.Blueprint);
         if (bp != null) {
           Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- {bp?.Path} ({string.Join(", ", bp?.Sources ?? new())})");
+          foreach (var spec in specService.GetSpecs<TemplateCollectionSpec>()) {
+            foreach (var blueprint in spec.Blueprints) {
+              if (blueprint.Path.Equals(bp?.Path, StringComparison.InvariantCultureIgnoreCase)) {
+                var bp3 = Singletons.BlueprintSourceService?.Get(spec.Blueprint);
+                Debug.Log(DateTime.Now.ToString("HH:mm:ss ") + $"  - {bp3?.Path} ({string.Join(", ", bp3?.Sources ?? new())})");
+              }
+            }
+          }
         } else {
           Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"- mystery location");
         }
