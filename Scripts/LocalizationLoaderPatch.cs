8e25ec50d04b1afbd4d0c7999f5fae85f9796ac2
Norman Rasmussen <norman@rasmussen.co.za>
2026-05-16 12:32:48 -0700

    Add an error if a localization value is null

--- Scripts/LocalizationLoaderPatch.cs
+++ Scripts/LocalizationLoaderPatch.cs
@@ -16,4 +16,13 @@ static class LocalizationLoaderPatch {
       Debug.LogWarning(DateTime.Now.ToString("HH:mm:ss ") + $"*** Localization {key} contains \"<s>\": {value}");
     }
   }
+
+  static void Finalizer(LocalizationLoader __instance, Exception __exception, string localizationKey) {
+    if (__exception == null) return;
+    foreach (LocalizationRecord item in __instance.GetDefaultLocalization()) {
+      if (item.Text is null) {
+        Debug.LogWarning(DateTime.Now.ToString("HH:mm:ss ") + $"*** Null localization key {item.Id} in {localizationKey}");
+      }
+    }
+  }
 }
