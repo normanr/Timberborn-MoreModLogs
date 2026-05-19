2f8eee12726ae0423fe6928d0c8944e3cceb5392
Norman Rasmussen <norman@rasmussen.co.za>
2026-05-19 00:21:50 -0700

    Add single argument string, int, double types to context for TextLocalizationWrapper update warning

--- Scripts/TextLocalizationWrapperPatch.cs
+++ Scripts/TextLocalizationWrapperPatch.cs
@@ -11,14 +11,17 @@ namespace Mods.MoreModLogs;
 static class TextLocalizationWrapperPatch {
 
   public static IEnumerable<MethodBase> TargetMethods() {
+    yield return SymbolExtensions.GetMethodInfo(() => default(TextLocalizationWrapper).WarnIfUpdating<string, object, object>);
+    yield return SymbolExtensions.GetMethodInfo(() => default(TextLocalizationWrapper).WarnIfUpdating<int, object, object>);
+    yield return SymbolExtensions.GetMethodInfo(() => default(TextLocalizationWrapper).WarnIfUpdating<double, object, object>);
     yield return SymbolExtensions.GetMethodInfo(() => default(TextLocalizationWrapper).WarnIfUpdating<int, string, object>);
     yield return SymbolExtensions.GetMethodInfo(() => default(TextLocalizationWrapper).WarnIfUpdating<int, int, object>);
   }
 
   static void Prefix(TextLocalizationWrapper __instance) {
     if (__instance._textLocalization == null) return;
-    var args = __instance._textLocalization.GetType().GenericTypeArguments;
-    Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"TextLocalizationWrapper: TextTemplate = {__instance._textTemplate}, Previous types: {args[0]} {args[1]}");
+    IEnumerable<Type> args = __instance._textLocalization.GetType().GenericTypeArguments;
+    Debug.LogError(DateTime.Now.ToString("HH:mm:ss ") + $"TextLocalizationWrapper: TextTemplate = {__instance._textTemplate}, Previous types: {string.Join(" ", args)}");
   }
 
 }
